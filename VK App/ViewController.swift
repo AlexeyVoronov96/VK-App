a5c0ff9990bc92ed377c8139a775e5a00eb13e5b
Alexey <alexey.voronov96@gmail.com>
2019-08-17 22:58:14 +0500

    Initial Commit

---
+++ VK App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VK App
+//
+//  Created by Алексей Воронов on 17.08.2019.
+//  Copyright © 2019 Алексей Воронов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
